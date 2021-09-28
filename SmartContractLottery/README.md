bd4c7e4032afaed149ec6a064d4a3a3195110422
DotNetRussell <anthonyrussellpublic@gmail.com>
2021-09-28 15:15:21 -0400

    Smart Contract Lottery additions

---
+++ SmartContractLottery/README.md
@@ -0,0 +1,4 @@
+1. Users can enter lottery with eth based on usd fee
+2. Admin will choose when lottery ends 
+ - Bonus, figure out how to use chainlink keepers
+3. Lottery will select random winner
---
+++ SmartContractLottery/contracts/Lottery.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+
+contract Lottery is VRFConsumerBase, Ownable {
+    address payable recentWinner;
+    address payable[] public players;
+    uint256 public usdEntryFee;
+    uint256 _randomness;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+
+    LOTTERY_STATE public lottery_state;
+
+    uint256 public _fee;
+    bytes32 public _keyhash;
+
+    constructor(
+        address priceFeedAddress,
+        address vrfCoordinator,
+        address link,
+        uint256 fee,
+        bytes32 keyhash
+    ) public VRFConsumerBase(vrfCoordinator, link) {
+        usdEntryFee = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        _fee = fee;
+        _keyhash = keyhash;
+    }
+
+    function enter() public payable {
+        require(lottery_state == LOTTERY_STATE.OPEN, "Lottery isn't open yet");
+        require(msg.value >= getEnteranceFee(), "Not enough ETH");
+        players.push(msg.sender);
+    }
+
+    function getEnteranceFee() public view returns (uint256) {
+        (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price) * 10**10;
+        uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
+        return costToEnter;
+    }
+
+    function startLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "A new Lottery can't be started"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
+
+    function endLottery() public onlyOwner {
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+        bytes32 requestId = requestRandomness(_keyhash, _fee);
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomness)
+        internal
+        override
+    {
+        require(
+            lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
+            "Lottery in wrong state"
+        );
+        require(randomness > 0, "Bad Random Number");
+        uint256 indexOfWinner = randomness % players.length;
+        recentWinner = players[indexOfWinner];
+
+        recentWinner.transfer(address(this).balance);
+
+        players = new address payable[](0);
+        lottery_state = LOTTERY_STATE.CLOSED;
+
+        _randomness = randomness;
+    }
+}
